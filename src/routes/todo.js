c79b9e0fd118defdd80466baafd6d1d6736bf622
Gonzalo Enzo Murua <gonzaloemurua96@gmail.com>
2023-04-28 17:51:59 -0300

    Edicion de update de tarea lista

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -4,7 +4,7 @@ const path = require('path');
 const pool = require('../../db');
 
 router.get('/', async (req, res) => {
-    const todo_list = await pool.query('SELECT * FROM todo_list');
+    const todo_list = await pool.query('SELECT * FROM todo_list WHERE done = 0;');
     res.render('todo/list', {todo_list});
 })
 
@@ -21,6 +21,14 @@ router.post('/add', async (req, res) => {
     };
     await pool.query('INSERT INTO todo_list SET ?', [form_data]);
     res.redirect('/todo');
-})
+});
+
+router.post('/update/:id', async (req, res) => {
+    const {id} = req.params;
+    console.log(`Id: ${id}`);
+    const done = true;
+    await pool.query('UPDATE todo_list SET done = ? WHERE id = ?', [done, id]);
+    res.redirect('/todo');
+});
 
 module.exports = router;
